5712856163d0566b229d20b46fd9fa4b26386166
Mazen Rashad <mazenrashadedu@gmail.com>
2024-12-20 18:43:18 +0200

    Fixing the navbar menu toggler

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 
 const root = createRoot(document.getElementById("root"));
 
